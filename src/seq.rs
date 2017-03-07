b417e3df9413498db3c6f28b3a6bd25db7f4d50a
SAWADA Tadashi <cesare@mayverse.jp>
2017-03-08 00:07:50 +0900

    Separate the arithmetic progression into the module

--- src/seq.rs
+++ src/seq.rs
@@ -1,30 +1,5 @@
-use std::ops::AddAssign;
-
-pub struct ArithmeticProgression<T> {
-    current: T,
-    diff: T,
-}
-
-impl<T> ArithmeticProgression<T> {
-    pub fn new(init: T, diff: T) -> ArithmeticProgression<T> {
-        ArithmeticProgression {
-            current: init,
-            diff: diff,
-        }
-    }
-}
-
-impl<T> Iterator for ArithmeticProgression<T>
-    where T: AddAssign + Copy
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<T> {
-        let current_value = self.current;
-        self.current += self.diff;
-        Some(current_value)
-    }
-}
+mod sequence;
+use sequence::ArithmeticProgression;
 
 fn main() {
     let ap = ArithmeticProgression::new(0, 2);
